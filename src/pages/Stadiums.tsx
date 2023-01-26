0576257358c7ed4f9d0d661b21b3fa4ca92be958
jean <jeanslucasilva@gmail.comm>
2023-01-26 15:29:32 -0500

    fetching data from stadiums

--- src/pages/Stadiums.tsx
+++ src/pages/Stadiums.tsx
@@ -1,6 +1,14 @@
-import { Menu } from "../components/Menu";
+import { useEffect } from 'react';
+import { Menu } from '../components/Menu';
+import { getData } from '../services/getData';
 
 export function Stadiums() {
+    useEffect(() => {
+        getData('https://services6.arcgis.com/ssFJjBXIUyZDrSYZ/arcgis/rest/services/Stadiums/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson')
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
         <>
             <Menu page='stadiums' />
