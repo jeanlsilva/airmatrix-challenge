9b9a193166690f0318963041903b3856166d4c2b
jean <jeanslucasilva@gmail.comm>
2023-01-26 15:32:20 -0500

    fetching data from special use airspace

--- src/pages/Special.tsx
+++ src/pages/Special.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 import { Menu } from "../components/Menu";
+import { getData } from "../services/getData";
 
 export function Special() {
+    useEffect(() => {
+        getData('https://services6.arcgis.com/ssFJjBXIUyZDrSYZ/arcgis/rest/services/Special_Use_Airspace/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson')
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }, []);
+    
     return (
         <>
             <Menu page='special' />
